097a9d2ecfa73aaabd27d84b5859d9a6ea4e8c4d
Renato Querino <renatoqrn@gmail.com>
2020-01-31 18:24:03 -0800

    starting Wolf3D

---
+++ includes/testing.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rquerino <rquerino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 16:37:58 by rquerino          #+#    #+#             */
+/*   Updated: 2020/01/31 16:40:13 by rquerino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTING_H
+# define TESTING_H
+
+# define mapWidth 24
+# define mapHeight 24
+# define screenWidth 640
+# define screenHeight 480
+
+#endif
